dc3ed24b387630586e23926bf0ca0bb7eeed681a
CarineIzere <carizeree@gmail.com>
2019-03-15 16:36:12 +0200

    add views

--- gallery/views.py
+++ gallery/views.py
@@ -2,91 +2,31 @@
 from .models import Image,Category,Location
 from django.http  import Http404
 from django.shortcuts import render,redirect
-# from .models import Image
+
 
 # Create your views here.
-def welcome(request):
-    # return HttpResponse('Welcome to my Gallery')
-    return render(request, 'welcome.html')
-def gallery_of_day(request):
-    date = dt.date.today()
-    news = Article.todays_gallery()
-    return render(request, 'all-images/today-images.html', {"date": date,})
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'index.html',{'images':images,'locations':locations})
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-def past_days_gallery(request,past_date):
-    # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-# def past_days_gallery(request,past_date):
-
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-#     except ValueError:
-#         # Raise 404 error when ValueError is thrown
-#         raise Http404()
-
-# View Function to present news from past days
-# def past_days_gallery(request, past_date):
-
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-#     except ValueError:
-#         # Raise 404 error when ValueError is thrown
-#         raise Http404()
-#         assert False
-
-#     if date == dt.date.today():
-#         return redirect(news_of_day)
-
-#     return render(request, 'all-news/past-news.html', {"date": date})
-
-
-# View Function to present news from past days
-def past_days_gallery(request, past_date):
-
+def display_location(request,location_id):
     try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
+        location = Location.objects.get(id = location_id)
+        images = Image.objects.filter(image_location = location.id)
+    except:
         raise Http404()
-        assert False
+    return render(request,'location.html',{'location':location,'images':images})
 
-    if date == dt.date.today():
-        return redirect(news_of_day)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+def search_category(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = (request.GET.get('category')).title()
+        searched_images = Image.search_by_category(search_term)
+        message = f'{search_term}'
+        return render(request,'search.html',{'message':message,'images':searched_images,'locations':locations})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request,'search.html',{'message':message,'locations':locations})
