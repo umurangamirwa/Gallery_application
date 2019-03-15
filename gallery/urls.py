4a00151a42f5dcac691824b2af147c2d781ecf8b
CarineIzere <carizeree@gmail.com>
2019-03-15 16:42:08 +0200

    add if condiotion in url gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.gallery_of_day,name='galleryToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery')
+    url(r'^$',views.index,name='index'),
+    url(r'^location/(\d+)',views.display_location,name='displayLocation'),
+    url(r'^search/',views.search_category,name='search_category'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
