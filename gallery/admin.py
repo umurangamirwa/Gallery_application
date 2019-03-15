93aa41758b69e8d73d2c7bc4fea67fcf2df5a1c7
CarineIzere <carizeree@gmail.com>
2019-03-15 12:11:56 +0200

    add sites

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 from .models import Editor,Article,tags
 from django.contrib import admin
 
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(tags)
 # Register your models here.
