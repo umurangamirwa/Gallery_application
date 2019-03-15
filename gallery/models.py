6ed7a0f13722cfba67e6236fba1079b01386da7c
CarineIzere <carizeree@gmail.com>
2019-03-15 12:48:27 +0200

    import date in models

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-
+import datetime as dt
 
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
