09c8f240cb6c041cbac6406dc55a25e73651dafe
CarineIzere <carizeree@gmail.com>
2019-03-15 12:32:06 +0200

    add function of article admin class

--- gallery/admin.py
+++ gallery/admin.py
@@ -6,6 +6,6 @@ from django.contrib import admin
 class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(tags)
 # Register your models here.
