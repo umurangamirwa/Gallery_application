1a527bd1dbd6850d4e468870d62d7362daf57f3c
CarineIzere <carizeree@gmail.com>
2019-03-15 12:29:31 +0200

    add article admin class

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from .models import Editor,Article,tags
 from django.contrib import admin
 
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
 admin.site.register(Editor)
 admin.site.register(Article)
 admin.site.register(tags)
